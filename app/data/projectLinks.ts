9cf440f292309612d29d76d4ad3729884587b3c2
MAGZOWN <magzown@gmail.com>
2026-07-06 21:21:32 +0500

    Create centralized project links configuration

---
+++ app/data/projectLinks.ts
@@ -0,0 +1,13 @@
+export const projectLinks = {
+  website: "https://coinmagbase.com",
+
+  explorer: "https://basescan.org",
+
+  tokenExplorer:
+    "https://basescan.org/token/0xbBd90410031Ed51023EF26Cca4e3e4f638F51A94",
+
+  contractExplorer:
+    "https://basescan.org/address/0xbBd90410031Ed51023EF26Cca4e3e4f638F51A94",
+
+  github: "https://github.com/MAGZOWN/nextjs-boilerplate",
+};
