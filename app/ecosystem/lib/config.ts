5fb9c1fc23198bd6c7f07c51cebe6f1ad5edec10
MAGZOWN <magzown@gmail.com>
2026-07-12 21:52:38 +0500

    Phase 15.2: Add official MAG Ecosystem configuration

---
+++ app/ecosystem/lib/config.ts
@@ -0,0 +1,20 @@
+export const MAG_CONTRACT_ADDRESS =
+  "0xbBd90410031Ed51023EF26Cca4e3e4f638F51A94" as const;
+
+export const MAG_TOKEN_NAME = "MAG Coin";
+export const MAG_TOKEN_SYMBOL = "MAG";
+export const MAG_TOKEN_DECIMALS = 18;
+export const MAG_TOTAL_SUPPLY = "1,000,000,000 MAG";
+
+export const BASE_CHAIN_ID = 8453;
+export const BASE_NETWORK_NAME = "Base Mainnet";
+
+export const MAG_BASESCAN_URL =
+  `https://basescan.org/token/${MAG_CONTRACT_ADDRESS}`;
+
+export const MAG_UNISWAP_POOL_URL =
+  "https://app.uniswap.org/explore/pools/base/0xfb1bb0105d24b563d83376e2d7425dd5e9652ca0";
+
+export const MAG_WEBSITE_URL = "https://coinmagbase.com";
+export const MAG_X_URL = "https://x.com/MAGCOINBASE";
+export const MAG_TELEGRAM_URL = "https://t.me/MAGCOINBASE";
